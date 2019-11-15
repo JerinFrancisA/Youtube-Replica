3f25d1dd6b677fd0d8d675b15e0b66cfbb51d118
JerinFrancisA <jerinfrancis77@gmail.com>
2019-11-15 22:50:25 +0530

    youtube icon

--- lib/you_tube_home.dart
+++ lib/you_tube_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class YouTubeHome extends StatelessWidget {
   @override
@@ -8,7 +9,22 @@ class YouTubeHome extends StatelessWidget {
         appBar: AppBar(
           title: Row(
             children: [
-
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    FontAwesomeIcons.youtubeSquare,
+                  ),
+                ),
+              ),
+              Text(
+                'YouTube',
+                style: TextStyle(
+                  wordSpacing: -1.5,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
             ],
           ),
         ),
