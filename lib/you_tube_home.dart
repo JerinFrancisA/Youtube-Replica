df31cb94839e134901598af72ab1d61247b75bb0
JerinFrancisA <jerinfrancis77@gmail.com>
2019-11-16 15:18:01 +0530

    bottm navbar

--- lib/you_tube_home.dart
+++ lib/you_tube_home.dart
@@ -18,10 +18,8 @@ class YouTubeHome extends StatelessWidget {
                   alignment: Alignment.centerLeft,
                   child: Row(
                     children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                        ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 5.0),
                         child: Icon(
                           FontAwesomeIcons.youtube,
                           color: Colors.red,
@@ -66,30 +64,31 @@ class YouTubeHome extends StatelessWidget {
               children: [
                 Video(
                   thumbnail:
-                  'https://i.ytimg.com/vi/5NxKNrfqUjs/maxresdefault.jpg',
+                      'https://i.ytimg.com/vi/5NxKNrfqUjs/maxresdefault.jpg',
                   title: 'SIDEMEN \$20,000 VS \$200 HOLIDAY (EUROPE EDITION)',
                   channelImage:
-                  'https://pbs.twimg.com/profile_images/1140596536205877248/2UYQ4X8a_400x400.jpg',
+                      'https://pbs.twimg.com/profile_images/1140596536205877248/2UYQ4X8a_400x400.jpg',
                   channel: 'Sidemen',
                   views: '12.3 M',
                   time: '1 month',
                 ),
                 Video(
                   thumbnail:
-                  'https://i.ytimg.com/vi/AFP-GlX3HjQ/maxresdefault.jpg',
+                      'https://i.ytimg.com/vi/AFP-GlX3HjQ/maxresdefault.jpg',
                   title: 'Queen - Bohemian Rhapsody HD',
                   channelImage:
-                  'https://yt3.ggpht.com/a/AGF-l7-hRQSYueq9V3KtUAlGUT-OYvpEXIduBulF_A=s900-c-k-c0xffffffff-no-rj-mo',
+                      'https://yt3.ggpht.com/a/AGF-l7-hRQSYueq9V3KtUAlGUT-OYvpEXIduBulF_A=s900-c-k-c0xffffffff-no-rj-mo',
                   channel: 'Queen Official',
                   views: '1 B',
                   time: '11 years',
                 ),
                 Video(
                   thumbnail:
-                  'https://i.ytimg.com/vi/pRpeEdMmmQ0/maxresdefault.jpg',
-                  title: 'Shakira - Waka Waka (This Time for Africa) (The Official 2010 FIFA World Cup™ Song)',
+                      'https://i.ytimg.com/vi/pRpeEdMmmQ0/maxresdefault.jpg',
+                  title:
+                      'Shakira - Waka Waka (This Time for Africa) (The Official 2010 FIFA World Cup™ Song)',
                   channelImage:
-                  'https://404store.com/2018/06/16/shakira-youtube-2017.jpg',
+                      'https://404store.com/2018/06/16/shakira-youtube-2017.jpg',
                   channel: 'Shakira',
                   views: '2.3 B',
                   time: '9 years',
@@ -98,7 +97,59 @@ class YouTubeHome extends StatelessWidget {
             ),
           ],
         ),
-        bottomNavigationBar: BottomNavigationBar(items:),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 0,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                size: 25.0,
+              ),
+              title: Text(
+                'Ho',
+                style: TextStyle(
+                  fontSize: 10.0,
+                ),
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add, size: 22.0),
+              title: Text(
+                'T',
+                style: TextStyle(
+                  fontSize: 10.0,
+                ),
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.subscriptions, size: 22.0),
+              title: Text(
+                'Sub',
+                style: TextStyle(
+                  fontSize: 10.0,
+                ),
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.mail, size: 24.0),
+              title: Text(
+                'Inbox',
+                style: TextStyle(
+                  fontSize: 10.0,
+                ),
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.video_library, size: 22.0),
+              title: Text(
+                'Lib',
+                style: TextStyle(
+                  fontSize: 10.0,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
